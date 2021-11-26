cd1b10a6c9fa3d51f3bad4b01798629fa083ce8e
Stephane <jo.steph2002@gmail.com>
2021-11-26 16:20:04 +0100

    salle de boss

--- js/aframe-component.js
+++ js/aframe-component.js
@@ -1,4 +1,5 @@
 var bombactive = false;
+var vie = 20;
 /* Permet de tirer */
 AFRAME.registerComponent('click-to-shoot', {
     init: function () {
@@ -11,9 +12,9 @@ AFRAME.registerComponent('click-to-shoot', {
 /* Joue la musique d'ambiance */
 document.addEventListener('click', musicPlay);
 function musicPlay() {
-  document.getElementById('musique').play();
-  document.removeEventListener('click', musicPlay);
-} 
+    document.getElementById('musique').play();
+    document.removeEventListener('click', musicPlay);
+}
 
 /* Son de l'arme quand elle tire */
 AFRAME.registerComponent('audiohandler', {
@@ -28,22 +29,38 @@ AFRAME.registerComponent('audiohandler', {
 
 AFRAME.registerComponent('collision', {
 
-    tick: function() {
-      function abs(val) {
-        return (val < 0) ? -val : val;
-      }
-      let pos = this.el.getAttribute("position");
-      
-      let posSphere = document.getElementById("boxTp").getAttribute("position");
-
-      if (abs(pos.x-posSphere.x) < 0.7 ) {
-        if(abs(pos.z-posSphere.z) < 0.7) {
-          this.el.setAttribute('position', { x: -2.76, y: 1.6, z:-2.1 });
-          let audio = document.querySelector("#sonTeleportation").play();
+    tick: function () {
+        function abs(val) {
+            return (val < 0) ? -val : val;
         }
-      }
+        let pos = this.el.getAttribute("position");
+
+        let posSphere = document.getElementById("boxTp").getAttribute("position");
+
+        if (abs(pos.x - posSphere.x) < 0.7) {
+            if (abs(pos.z - posSphere.z) < 0.7) {
+                this.el.setAttribute('position', { x: -2.76, y: 1.6, z: -2.1 });
+                let audio = document.querySelector("#sonTeleportation").play();
+
+            }
+        }
     }
-  });
+});
+
+AFRAME.registerComponent('tpsalleboss', {
+
+    tick: function () {
+        document.getElementById('skull2').addEventListener('click', function (evt) {
+            document.getElementById('skull2').setAttribute('animation', {
+                property: 'position',
+                to: '25.4 1.8 -13.417'
+            });
+            document.getElementById('skull2').setAttribute('link', 'href:boss.html')
+
+
+        });
+    }
+});
 
 AFRAME.registerComponent('trackball', {
     tick: function () {
@@ -60,8 +77,8 @@ AFRAME.registerComponent('trackball', {
         if (abs(pos.x - posSphere.x) < 4) {
             if (abs(pos.z - posSphere.z) < 4) {
                 bombactive = true;
-                document.getElementById('musique').pause(); 
-                document.getElementById('countdown').play(); 
+                document.getElementById('musique').pause();
+                document.getElementById('countdown').play();
                 document.getElementById("tbombe").setAttribute("visible", "true");
                 document.getElementById("compteur").setAttribute("visible", "true");
 
@@ -91,12 +108,12 @@ AFRAME.registerComponent('trackball', {
 
 
                 var fiveMinutes = 30,
-                display = document.getElementById("time2");
+                    display = document.getElementById("time2");
                 startTimer(fiveMinutes, display);
 
                 document.getElementById('interrupteur2').addEventListener('click', function () {
                     document.getElementById('countdown').pause();
-                    document.getElementById('musique').play(); 
+                    document.getElementById('musique').play();
                     document.getElementById("compteur").setAttribute("visible", "false");
                     document.getElementById('tinterrupteur').setAttribute('visible', "true");
                 });
@@ -136,7 +153,6 @@ AFRAME.registerComponent('hit-handler', {
         var el = this.el;
 
         el.addEventListener('hit', () => {
-            
         });
 
         el.addEventListener('die', () => {
@@ -155,5 +171,97 @@ AFRAME.registerComponent('shoot-ennemy', {
         setInterval(function () {
             enemy.emit('shoot');
         }, 1000);
+    }
+});
+AFRAME.registerComponent('shoot-ennemy-boss', {
+    init: function () {
+        let enemy = this.el;
+        setInterval(function () {
+            enemy.emit('shoot');
+        }, 1800);
+    }
+});
+AFRAME.registerComponent('shoot-ennemy-rafale', {
+    init: function () {
+        let enemy = this.el;
+        setInterval(function () {
+            enemy.emit('shoot');
+        }, 2500);
+    }
+});
+AFRAME.registerComponent('hit-handler-boss', {
+    dependencies: ['material'],
+
+    init: function () {
+        var rotationTmp = this.rotationTmp = this.rotationTmp || { x: 0, y: 0, z: 0 };
+        var el = this.el;
+        var missile = document.getElementById('missile2')
+        var sphere1 = document.getElementById('sphere1');
+        var sphere2 = document.getElementById('sphere2');
+        var sphere3 = document.getElementById('sphere3');
+        var sphere4 = document.getElementById('sphere4');
+        var sphere5 = document.getElementById('sphere5');
+        var sphere6 = document.getElementById('sphere6');
+        var sphere7 = document.getElementById('sphere7');
+        var sphere8 = document.getElementById('sphere8');
+        var sphere9 = document.getElementById('sphere9');
+        var sphere10 = document.getElementById('sphere10');
+        var sphere11 = document.getElementById('sphere11');
+        var sphere12 = document.getElementById('sphere12');
+
+        el.addEventListener('hit', () => {
+            document.getElementById('bosslife').setAttribute('geometry', {
+                width: vie
+            });
+            if(vie<10){
+                document.getElementById('bosslife').setAttribute('material','color:orange')
+                missile.setAttribute('visible','true')
+                missile.setAttribute('animation',{
+                    property:'position',
+                    to:'0 0 -1',
+                    dur:3000
+                });
+
+                
+            }
+            if(vie<5){
+                document.getElementById('bosslife').setAttribute('material','color:red')
+                sphere5.setAttribute('shoot-ennemy-rafale', null)
+                sphere6.setAttribute('shoot-ennemy-rafale', null)
+                sphere7.setAttribute('shoot-ennemy-rafale', null)
+                sphere8.setAttribute('shoot-ennemy-rafale', null)
+                sphere9.setAttribute('shoot-ennemy-rafale', null)
+                sphere10.setAttribute('shoot-ennemy-rafale', null)
+                sphere11.setAttribute('shoot-ennemy-rafale', null)
+                sphere12.setAttribute('shoot-ennemy-rafale', null)
+            }
+            if (vie < 0) {
+                el.parentNode.remove(el);
+                sphere1.parentNode.remove(sphere1);
+                sphere2.parentNode.remove(sphere2);
+                sphere3.parentNode.remove(sphere3);
+                sphere4.parentNode.remove(sphere4);
+
+            }
+            vie = vie - 0.04
+            
+            
+        });
+
+        el.addEventListener('die', () => {
+
+            var rotation = el.getAttribute('position');
+            rotationTmp.x = rotation.x + 0.1;
+            rotationTmp.y = rotation.y - 100000;
+            rotationTmp.z = rotation.z + 0.1;
+            el.setAttribute('position', rotationTmp);
+        });
+    }
+});
+AFRAME.registerComponent('monster-roar', {
+    init: function () {
+        setInterval(function () {
+            document.getElementById('roar').play();
+        }, 20000);
     }
 });
