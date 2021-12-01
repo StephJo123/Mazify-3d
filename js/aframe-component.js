1489f5598aab9c773e91f40003bac34692c1c75e
Thibaud <thibaud.bonneville@gmail.com>
2021-12-01 17:07:22 +0100

    merge

--- js/aframe-component.js
+++ js/aframe-component.js
@@ -357,25 +357,26 @@ AFRAME.registerComponent('ghost-follow', {
   init: function () {
     let ghost = this.el;
     let nbRotate = 0;
-    function avance(ghost,pas) {
-        let rotation = ghost.object3D.rotation;
-        let pos = ghost.object3D.position;
-        if (Math.abs(rotation.y + 1.5708) < 0.2) {
-            ghost.object3D.position.set(pos.x - pas, pos.y, pos.z);
-        } else if (Math.abs(rotation.y - 1.5708) < 0.2) {
-            ghost.object3D.position.set(pos.x + pas, pos.y, pos.z);
-        } else {
-           ghost.object3D.position.set(pos.x,pos.y,pos.z += (rotation.z >= -0.2 && rotation.z <= 0.2) ? pas : -pas); 
-        }
+
+    function avance(ghost, pas) {
+      let rotation = ghost.object3D.rotation;
+      let pos = ghost.object3D.position;
+      if (Math.abs(rotation.y + 1.5708) < 0.2) {
+        ghost.object3D.position.set(pos.x - pas, pos.y, pos.z);
+      } else if (Math.abs(rotation.y - 1.5708) < 0.2) {
+        ghost.object3D.position.set(pos.x + pas, pos.y, pos.z);
+      } else {
+        ghost.object3D.position.set(pos.x, pos.y, pos.z += (rotation.z >= -0.2 && rotation.z <= 0.2) ? pas : -pas);
+      }
     }
     setInterval(function () {
-        avance(ghost,1); // on avance pour savoir si la position sera dans le mur
-        if (!isValidePosition(ghost.object3D.position)) { // s'il est dans un mur
-            avance(ghost,-1); // on revient avant de tourner
-            ghost.object3D.rotateY((Math.random() <= 0.5) ? 1.5708 : -1.5708); // on tourne
-        }
-    },500);
-    ghost.object3D.rotation.set(0,-1.5708,0);
+      avance(ghost, 1); // on avance pour savoir si la position sera dans le mur
+      if (!isValidePosition(ghost.object3D.position)) { // s'il est dans un mur
+        avance(ghost, -1); // on revient avant de tourner
+        ghost.object3D.rotateY((Math.random() <= 0.5) ? 1.5708 : -1.5708); // on tourne
+      }
+    }, 500);
+    ghost.object3D.rotation.set(0, -1.5708, 0);
   }
 });
 
