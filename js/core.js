0660dad57399ca6e2ac8274ab9380e0d36e4680f
Thibaud <thibaud.bonneville@gmail.com>
2021-12-09 11:30:30 +0100

    ok

--- js/core.js
+++ js/core.js
@@ -2,7 +2,7 @@ function $(v) {
   return document.getElementById(v);
 }
 
-var isValidePosition = function(posInit) {
+var isValidePosition = function (posInit) {
   let bool = true;
   lesmurs.forEach(function (el) {
     const posN = el.object3D.position;
@@ -14,49 +14,52 @@ var isValidePosition = function(posInit) {
   return bool;
 }
 
-var removeIfExist = function(element) {
+var removeIfExist = function (element) {
   if (document.body.contains(element)) {
     element.remove();
   }
 }
 
-var randomIntFromInterval = function(min, max) {
+var randomIntFromInterval = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var toggleCursorColor = function(el) {
+var toggleCursorColor = function (el) {
   el.addEventListener('mouseenter', () => cursor.setAttribute('material', 'color: springgreen'));
   el.addEventListener('mouseleave', () => cursor.setAttribute('material', 'color: white'));
 }
 
-var removeIfVrOrNot = function(target) {
-	let cs = document.querySelectorAll(target);
-	if (cs.length == 2) {
-		(AFRAME.utils.device.checkHeadsetConnected()) ? cs[1].remove() : cs[0].remove();
-	}
-}
-
 AFRAME.registerComponent('reticule-position', {
-	init: function() {
-		removeIfVrOrNot('a-cursor');
-		removeIfVrOrNot('a-entity.compteur');
-		document.querySelector('a-entity.compteur').setAttribute('id','compteur');
-	},
-    tick: function() {
-		if (AFRAME.utils.device.checkHeadsetConnected()) {
-			const ogun = $('gun-model');
-			const gun = (ogun) ? ogun.object3D : $('handGun').object3D;
-			const posGun = gun.getWorldPosition();
-			const directionGun = gun.getWorldDirection();
-			this.el.object3D.position.set(posGun.x-1*directionGun.x,posGun.y-1*directionGun.y,posGun.z-1*directionGun.z);
-		}
-		const acomp = $('compteur');
-		if (AFRAME.utils.device.checkHeadsetConnected() && acomp != null){
-			const handLeft = $('gauche').object3D;
-			let t = new THREE.Vector3();
-			const posHand = handLeft.getWorldPosition(t);
-			handLeft.getWorldDirection(t);
-			acomp.object3D.position.set(posHand.x,posHand.y+0.2,posHand.z);
-		}
+  init: function () {
+    function removeIfVrOrNot(target) {
+      let cs = document.querySelectorAll(target);
+      if (cs.length == 2) {
+        (AFRAME.utils.device.checkHeadsetConnected()) ? cs[1].remove(): cs[0].remove();
+      }
     }
+    removeIfVrOrNot('a-cursor');
+  },
+  tick: function () {
+    if (AFRAME.utils.device.checkHeadsetConnected()) {
+      const ogun = $('gun-model');
+      const gun = (ogun) ? ogun.object3D : $('handGun').object3D;
+      const posGun = gun.getWorldPosition();
+      const directionGun = gun.getWorldDirection();
+      $('cursor').object3D.position.set(posGun.x - 1 * directionGun.x, posGun.y - 1 * directionGun.y, posGun.z - 1 * directionGun.z);
+    }
+    const acomp = $('compteur');
+    if (AFRAME.utils.device.checkHeadsetConnected() && acomp != null) {
+      const handLeft = $('gauche').object3D;
+      let t = new THREE.Vector3();
+      const posHand = handLeft.getWorldPosition(t);
+      handLeft.getWorldDirection(t);
+      acomp.object3D.position.set(posHand.x, posHand.y + 0.2, posHand.z);
+    }
+  }
+});
+// ATTENTION POUR L'UTILISER VOUS DEVEZ AVOIR DEUX a-entity avec la classe 'compteur'
+AFRAME.registerComponent('id-compteur-vr', {
+  init: function () {
+    document.querySelectorAll('a-entity.compteur')[(AFRAME.utils.device.checkHeadsetConnected()) ? 0 : 1].setAttribute('id', 'compteur');
+  }
 });
